33fdcb2ef648248b2b6cb5d13022cc68b51e745a
Dominic Bett <dominicbett@hotmail.com>
2015-04-11 23:35:49 -0400

    Google Code Jam 2015

---
+++ D_OminousOmino.py
@@ -0,0 +1,40 @@
+
+import sys
+def ominousOmino(input_, T):
+	
+	a = "GABRIEL"
+	b = "RICHARD"
+	for t in range(0, T):
+		X, R, C = input_[t].split()
+		X = int(X)
+		R = int(R)
+		C = int(C)
+		y = X
+		status = ""
+		for x in range(1,X+1):
+			if (R>=y and C>=x) or (R>=x and C>=y):
+				status = "pass"
+			else:
+				status = "fail"
+				break
+			y -= 1
+		if (R*C)%X==0 and (0.5*X)<=R and (0.5*X<=C) and (R*C)/X>1 and status=="pass":
+			r = "Case #%d: %s"%(t+1, a)
+		else:
+			r = "Case #%d: %s"%(t+1, b)
+		print r
+
+if __name__ == "__main__":
+    f = sys.stdin
+    if len(sys.argv) >= 2:
+        fn = sys.argv[1]
+        if fn != '-':
+            f = open(fn)
+
+    t = int(f.readline())
+    inp = []
+    for _t in xrange(t):
+        line =  f.readline().strip()
+        inp.append(line)
+
+    ominousOmino(input_=inp, T=t)
